99894868ea112cbaa15fac7ea5a28d438438bb9b
Kiran Murmu <44278642+KiranMurmuTH@users.noreply.github.com>
2018-11-01 22:15:48 +0530

    Update useful-scripts/auto-hide-ad-for-kissmanga.js

--- useful-scripts/auto-hide-ad-for-kissmanga.js
+++ useful-scripts/auto-hide-ad-for-kissmanga.js
@@ -1,6 +1,6 @@
-var _css_element_selector_array = ['#tab-newest > div:first-child', '#divAds2', '#rightside > div:nth-child(5) > div'];
-var _css_new_style_element = document.createElement('style');
+var _css_element_selector_array = ["div > iframe[src*='ads']", "div > a[href*='click']"];
+var _css_new_style_element = document.createElement("style");
 for (i = 0; i < _css_element_selector_array.length; i++) {
-    _css_new_style_element.innerHTML += _css_element_selector_array[i] + '{display: none;}';
+    _css_new_style_element.innerHTML += _css_element_selector_array[i] + "{display: none;}";
 }
 document.head.appendChild(_css_new_style_element);
