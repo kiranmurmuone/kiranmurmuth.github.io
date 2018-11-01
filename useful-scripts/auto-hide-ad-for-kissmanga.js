4d7dab3d07631ad32cfee491edf90d036d874092
Kiran Murmu <44278642+KiranMurmuTH@users.noreply.github.com>
2018-11-01 20:38:18 +0530

    Add files via upload

---
+++ useful-scripts/auto-hide-ad-for-kissmanga.js
@@ -0,0 +1,8 @@
+var _css_element_selector_array = ['#tab-newest > div:first-child', '#divAds2'];
+var _css_new_style_element = document.createElement('style');
+
+for (i = 0; i < _css_element_selector_array.length; i++) {
+    _css_new_style_element.innerHTML += '\n\t' + _css_element_selector_array[i] + ' {\n\t\tdisplay: none;\n\t}';
+}
+_css_new_style_element.innerHTML += '\n';
+document.head.appendChild(_css_new_style_element);
