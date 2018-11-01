20ba87328f6691e4f27e77540c62df7f0a11b93b
Kiran Murmu <44278642+KiranMurmuTH@users.noreply.github.com>
2018-11-01 20:45:12 +0530

    Update auto-hide-ad-for-kissmanga.js

--- useful-scripts/auto-hide-ad-for-kissmanga.js
+++ useful-scripts/auto-hide-ad-for-kissmanga.js
@@ -1,4 +1,4 @@
-var _css_element_selector_array = ['#tab-newest > div:first-child', '#divAds2'];
+var _css_element_selector_array = ['#tab-newest > div:first-child', '#divAds2', 'clear', 'clear2'];
 var _css_new_style_element = document.createElement('style');
 
 for (i = 0; i < _css_element_selector_array.length; i++) {
