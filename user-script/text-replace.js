a3b072c2fd8dbe9c06d5e0bc2337a74eb62c9c5f
Kiran Murmu <44278642+kiranmurmuth@users.noreply.github.com>
2019-07-30 04:43:30 +0530

    Update text-replace.js

--- user-script/text-replace.js
+++ user-script/text-replace.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Text Replace
 // @namespace    io.github.kiranmurmuth.user-script.text-replace
-// @version      1.0.4
+// @version      1.0.5
 // @description  A basic text replacer for webpages.
 // @author       KiranMurmuTH
 // @run-at       document-end
@@ -9,16 +9,15 @@
 
 (function() {
     'use strict';
-	document.onkeyup = function (e) {
-		if (e.ctrlKey && e.altKey && e.which == 82) //ctrl + alt + R
+	document.onkeydown = function (e) {
+		//ctrl + alt + R
+		if (e.ctrlKey && e.altKey && e.which == 82)
 		{
 			var find_what, replace_with;
-			find_what = prompt("Replace what ?");
-			if (find_what) {
-				replace_with = prompt("Replace with:");
-				if (replace_with) {
-					document.body.innerHTML = document.body.innerHTML.split(find_what).join(replace_with);
-				}
+			find_what = prompt("Replace what in this page?");
+			if (find_what && find_what.length > 0) {
+				replace_with = prompt("Replace all [ " + find_what + " ] with:");
+				document.body.innerHTML = document.body.innerHTML.split(find_what).join(replace_with);
 			}
 		}
 		document.getElementsByTagName("BODY")[0].focus();
