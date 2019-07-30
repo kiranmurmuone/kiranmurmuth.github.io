01f61da246ce89ad70f7e207f66f30182189cced
Kiran Murmu <44278642+kiranmurmuth@users.noreply.github.com>
2019-07-30 23:48:51 +0530

    Update 1.0.6
    -fixed "null" replace when cancelling.
    -changed hotkeys to ctrl + shift + R

--- user-script/text-replace.js
+++ user-script/text-replace.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Text Replace
 // @namespace    io.github.kiranmurmuth.user-script.text-replace
-// @version      1.0.5
+// @version      1.0.6
 // @description  A basic text replacer for webpages.
 // @author       KiranMurmuTH
 // @run-at       document-end
@@ -10,14 +10,16 @@
 (function() {
     'use strict';
 	document.onkeydown = function (e) {
-		//ctrl + alt + R
-		if (e.ctrlKey && e.altKey && e.which == 82)
+		//ctrl + shift + R
+		if (e.ctrlKey && e.shiftKey && e.which == 82)
 		{
 			var find_what, replace_with;
 			find_what = prompt("Replace what in this page?");
-			if (find_what && find_what.length > 0) {
-				replace_with = prompt("Replace all [ " + find_what + " ] with:");
-				document.body.innerHTML = document.body.innerHTML.split(find_what).join(replace_with);
+			if (find_what != null && find_what.length != 0) {
+                replace_with = prompt("Replace all \" " + find_what + " \" with:");
+                if (replace_with != null) {
+                    document.body.innerHTML = document.body.innerHTML.split(find_what).join(replace_with);
+                }
 			}
 		}
 		document.getElementsByTagName("BODY")[0].focus();
