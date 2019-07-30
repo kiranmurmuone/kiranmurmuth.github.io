97a958429c3b884163622f5ff3d0fb0a168c1eb1
Kiran Murmu <44278642+kiranmurmuth@users.noreply.github.com>
2019-07-31 02:39:37 +0530

    Update 1.0.7
    -fixed internal text replace.

--- user-script/text-replace.js
+++ user-script/text-replace.js
@@ -13,12 +13,18 @@
 		//ctrl + shift + H
 		if (e.ctrlKey && e.shiftKey && e.which == 72)
 		{
-			var find_what, replace_with;
+			var find_what, replace_with, text_node, snap_shot, snap_data;
 			find_what = prompt("Replace what in this page?");
 			if (find_what != null && find_what.length != 0) {
 				replace_with = prompt("Replace all \" " + find_what + " \" with:");
 				if (replace_with != null) {
-					document.body.innerHTML = document.body.innerHTML.split(find_what).join(replace_with);
+					text_node = document.evaluate( "//body//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+					for (var snap_index = 0; snap_index < text_node.snapshotLength; snap_index++) {
+						snap_shot = text_node.snapshotItem(snap_index);
+						snap_data = snap_shot.data;
+						snap_data = snap_data.replace(find_what, replace_with);
+						snap_shot.data = snap_data;
+					}
 				}
 			}
 		}
